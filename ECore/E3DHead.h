ca903b7370f3a13813b979d656e385b4cc77efb5
张宏杰 <zhanghongjie@ztgame.com>
2019-10-25 14:59:37 +0800

    删除多余声明

--- ECore/E3DHead.h
+++ ECore/E3DHead.h
@@ -7,7 +7,7 @@ namespace E3D
 	struct EVector4D;	//四维向量
 	struct EMatrix44;	//4X4矩阵
 
-	struct EVector4D;	//顶点
+	struct EVertex4D;	//顶点
 	struct EPolyon4D;	//基于顶点多边形
 	struct EPolyonF4D;	//基于索引多边形
 	struct EObject4D;	//对象
